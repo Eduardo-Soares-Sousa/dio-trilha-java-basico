30aaac0711a3b9e2d62b32b15029e777cf942c96
Eduardo Soares <eduardo_soares001@outlook.com>
2024-05-14 13:57:46 -0300

    Término da documentação

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,12 +1,30 @@
+/**
+ * <h1>Contador</h1>
+ * O contador recebe dois parâmetros via terminal que representarão dois números inteiros, 
+ * com estes dois números você deverá obter a quantidade de interações (for) e realizar a impressão no console dos números incrementados
+ * 
+ * @author Eduardo Soares de Sousa
+ * @version 1.0
+ * @since 14/05/2024
+ */
+
 import java.util.Scanner;
 
 public class Contador {
+    /**
+     * Este método é o ponto de entrada para a execução do programa.
+     * @param args Argumentos da linha de comando (opcional).
+     */
     public static void main(String[] args) throws Exception {
         Scanner terminal = new Scanner(System.in);
+
 		System.out.println("Digite o primeiro parâmetro");
 		int parametroUm = terminal.nextInt();
+        terminal.nextLine();
+
 		System.out.println("Digite o segundo parâmetro");
 		int parametroDois = terminal.nextInt();
+        terminal.nextLine();
 
         try{
             contar(parametroUm, parametroDois);
@@ -15,12 +33,19 @@ public class Contador {
         }
     }
 
+    /**
+    * Este método realiza uma operação que pode gerar uma exceção.
+    * 
+    * @param parametroUm O primeiro parâmetro da operação.
+    * @param parametroDois O segundo parâmetro da operação.
+    * @throws ParametrosInvalidosException Se os parâmetros forem inválidos.
+    */
     public static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
         if(parametroUm > parametroDois){
             throw new ParametrosInvalidosException();
         }else{
             int contagem = parametroDois - parametroUm;
-            for(int i = contagem; i >= parametroDois; i++){
+            for(int i = 1; i <= contagem; i++){
                 System.out.println("Imprimindo o número " + i);
             }
         }
--- DesafioControleFluxo/src/ParametrosInvalidosException.java
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -1,8 +1,19 @@
+/**
+ * Exceção lançada quando o primeiro parâmetro for maior que o segundo parâmetro.
+ */
 public class ParametrosInvalidosException extends RuntimeException{
+    /**
+    * Cria uma nova exceção com uma mensagem padrão.
+    * A mensagem padrão é: "O segundo parâmetro deve ser maior que o primeiro".
+    */
     public ParametrosInvalidosException(){
         super("O segundo parâmetro deve ser maior que o primeiro");
     }
 
+    /**
+    * Cria uma nova exceção com a mensagem especificada.
+    * @param mensagem A mensagem de erro específica para esta exceção.
+    */
     public ParametrosInvalidosException(String mensagem){
         super(mensagem);
     }
