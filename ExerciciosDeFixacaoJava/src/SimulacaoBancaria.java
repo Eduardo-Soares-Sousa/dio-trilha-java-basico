e8ab03ae917990613ab5bffb1d905b6f573548c1
Eduardo Soares <eduardo_soares001@outlook.com>
2024-05-16 09:02:26 -0300

    Implementação dos exercícios

---
+++ ExerciciosDeFixacaoJava/src/SimulacaoBancaria.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class SimulacaoBancaria {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        double saldo = 0;
+        boolean sair = false;
+
+        while(sair != true){
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(opcao){
+                case 1:
+                    double depositar = scanner.nextDouble();
+                    scanner.nextLine();
+
+                    saldo += depositar;
+                    System.out.println("Saldo atual: " + String.format("%.1f", saldo)); 
+                    break;
+
+                case 2:
+                    double sacar = scanner.nextDouble();
+                    scanner.nextLine();
+
+                    if(sacar < saldo){
+                        saldo -= sacar;
+                        System.out.println("Saldo atual: " + String.format("%.1f", saldo)); 
+                    }else{
+                        System.out.println("Saldo insuficiente.");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Saldo atual: " + String.format("%.1f", saldo));
+                    break;
+
+                case 0:
+                    System.out.println("Programa encerrado.");
+                    sair = true;
+                    break;
+
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+                    break;
+            }
+        }
+    }
+}
